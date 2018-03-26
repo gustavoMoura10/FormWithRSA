cc9ee68fcb1218aa92393e387ad847708f1839cd
Gustavo <gustavomouraunip@gmail.com>
2018-03-25 21:48:19 -0300

    Formulario com criptografia RSA

---
+++ WindowsFormsApp/WindowsFormsApp2/Form1.Designer.cs
@@ -0,0 +1,311 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.botao1 = new System.Windows.Forms.Button();
+            this.botao2 = new System.Windows.Forms.Button();
+            this.numer = new System.Windows.Forms.NumericUpDown();
+            this.text1 = new System.Windows.Forms.TextBox();
+            this.text2 = new System.Windows.Forms.TextBox();
+            this.text3 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.keye = new System.Windows.Forms.Label();
+            this.keyn = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.keyp = new System.Windows.Forms.Label();
+            this.keyq = new System.Windows.Forms.Label();
+            this.keyde = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numer)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // botao1
+            // 
+            this.botao1.Location = new System.Drawing.Point(234, 290);
+            this.botao1.Name = "botao1";
+            this.botao1.Size = new System.Drawing.Size(75, 23);
+            this.botao1.TabIndex = 0;
+            this.botao1.Text = "Criptografar";
+            this.botao1.UseVisualStyleBackColor = true;
+            this.botao1.Click += new System.EventHandler(this.botao1_Click);
+            // 
+            // botao2
+            // 
+            this.botao2.Location = new System.Drawing.Point(234, 402);
+            this.botao2.Name = "botao2";
+            this.botao2.Size = new System.Drawing.Size(75, 23);
+            this.botao2.TabIndex = 1;
+            this.botao2.Text = "Descriptografar";
+            this.botao2.UseVisualStyleBackColor = true;
+            this.botao2.Click += new System.EventHandler(this.botao2_Click);
+            // 
+            // numer
+            // 
+            this.numer.Location = new System.Drawing.Point(219, 25);
+            this.numer.Name = "numer";
+            this.numer.Size = new System.Drawing.Size(105, 20);
+            this.numer.TabIndex = 2;
+            this.numer.Value = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.numer.ValueChanged += new System.EventHandler(this.numer_ValueChanged);
+            // 
+            // text1
+            // 
+            this.text1.Location = new System.Drawing.Point(30, 209);
+            this.text1.Multiline = true;
+            this.text1.Name = "text1";
+            this.text1.Size = new System.Drawing.Size(494, 75);
+            this.text1.TabIndex = 3;
+            // 
+            // text2
+            // 
+            this.text2.Location = new System.Drawing.Point(30, 319);
+            this.text2.Multiline = true;
+            this.text2.Name = "text2";
+            this.text2.Size = new System.Drawing.Size(494, 77);
+            this.text2.TabIndex = 4;
+            // 
+            // text3
+            // 
+            this.text3.Location = new System.Drawing.Point(30, 431);
+            this.text3.Multiline = true;
+            this.text3.Name = "text3";
+            this.text3.Size = new System.Drawing.Size(494, 67);
+            this.text3.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(488, 504);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Limpar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(195, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(163, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Escolha um número de 10  a 100";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(340, 68);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(75, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Chave pública";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(421, 51);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(14, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "E";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(485, 51);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(15, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "N";
+            // 
+            // keye
+            // 
+            this.keye.AutoSize = true;
+            this.keye.Location = new System.Drawing.Point(421, 68);
+            this.keye.Name = "keye";
+            this.keye.Size = new System.Drawing.Size(13, 13);
+            this.keye.TabIndex = 11;
+            this.keye.Text = "0";
+            this.keye.Click += new System.EventHandler(this.e_Click);
+            // 
+            // keyn
+            // 
+            this.keyn.AutoSize = true;
+            this.keyn.Location = new System.Drawing.Point(485, 68);
+            this.keyn.Name = "keyn";
+            this.keyn.Size = new System.Drawing.Size(13, 13);
+            this.keyn.TabIndex = 12;
+            this.keyn.Text = "0";
+            this.keyn.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(340, 131);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(76, 13);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Chave privada";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(422, 118);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(14, 13);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "P";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(465, 118);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(15, 13);
+            this.label8.TabIndex = 15;
+            this.label8.Text = "Q";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(526, 118);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(28, 13);
+            this.label9.TabIndex = 16;
+            this.label9.Text = "D(E)";
+            // 
+            // keyp
+            // 
+            this.keyp.AutoSize = true;
+            this.keyp.Location = new System.Drawing.Point(423, 135);
+            this.keyp.Name = "keyp";
+            this.keyp.Size = new System.Drawing.Size(13, 13);
+            this.keyp.TabIndex = 17;
+            this.keyp.Text = "0";
+            // 
+            // keyq
+            // 
+            this.keyq.AutoSize = true;
+            this.keyq.Location = new System.Drawing.Point(468, 135);
+            this.keyq.Name = "keyq";
+            this.keyq.Size = new System.Drawing.Size(13, 13);
+            this.keyq.TabIndex = 18;
+            this.keyq.Text = "0";
+            // 
+            // keyde
+            // 
+            this.keyde.AutoSize = true;
+            this.keyde.Location = new System.Drawing.Point(529, 135);
+            this.keyde.Name = "keyde";
+            this.keyde.Size = new System.Drawing.Size(13, 13);
+            this.keyde.TabIndex = 19;
+            this.keyde.Text = "0";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(69, 193);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(431, 13);
+            this.label5.TabIndex = 20;
+            this.label5.Text = "Obs: Pedimos para que seja evitado o uso de acentos para uma leitura boa da cript" +
+    "ografia";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(575, 539);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.keyde);
+            this.Controls.Add(this.keyq);
+            this.Controls.Add(this.keyp);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.keyn);
+            this.Controls.Add(this.keye);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.text3);
+            this.Controls.Add(this.text2);
+            this.Controls.Add(this.text1);
+            this.Controls.Add(this.numer);
+            this.Controls.Add(this.botao2);
+            this.Controls.Add(this.botao1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numer)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button botao1;
+        private System.Windows.Forms.Button botao2;
+        private System.Windows.Forms.NumericUpDown numer;
+        private System.Windows.Forms.TextBox text1;
+        private System.Windows.Forms.TextBox text2;
+        private System.Windows.Forms.TextBox text3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label keye;
+        private System.Windows.Forms.Label keyn;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label keyp;
+        private System.Windows.Forms.Label keyq;
+        private System.Windows.Forms.Label keyde;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ WindowsFormsApp/WindowsFormsApp2/Form1.cs
@@ -0,0 +1,231 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Numerics;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        string msg1 = "";
+        string msg2 = "";
+        string msgcnv1 = "";
+        string msgcnv2 = "";
+        BigInteger ascII1;
+        BigInteger ascII2;
+        BigInteger num = 0;
+        BigInteger p = 0;
+        BigInteger q = 0;
+        BigInteger totiente = 0;
+        BigInteger inv = 0;
+        BigInteger cript = 0;
+        BigInteger n = 0;
+        BigInteger ev = 0;
+        BigInteger desc = 0;
+        double[] aloc;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //Função para determinar qual número primo o p será
+        public static BigInteger pPrime(BigInteger number)
+        {
+            BigInteger p = 0;
+            bool isPrime = true;
+            for (int i = 2; i <= number; i++)
+            {
+                for (int j = 2; j <= number; j++)
+                {
+
+                    if (i != j && i % j == 0)
+                    {
+                        isPrime = false;
+                        break;
+                    }
+
+                }
+                if (isPrime)
+                {
+                    p = i;
+                }
+                isPrime = true;
+            }
+            return p;
+        }
+       //determinara qual número primo o q será
+        public static BigInteger qPrime(BigInteger number)
+        {
+            BigInteger q = 0;
+            bool isPrime = true;
+            for (int i = 2; i <= number * 2; i++)
+            {
+                for (int j = 2; j <= number; j++)
+                {
+
+                    if (i != j && i % j == 0)
+                    {
+                        isPrime = false;
+                        break;
+                    }
+
+                }
+                if (isPrime)
+                {
+                    q = i;
+                }
+                isPrime = true;
+            }
+            return q;
+        }
+        //Aqui ocorrerá a escolha o inverso modular
+        public static BigInteger modInverse(BigInteger a, BigInteger n)
+        {
+            BigInteger i = n, v = 0, d = 1;
+            while (a > 0)
+            {
+                BigInteger t = i / a, x = a;
+                a = i % x;
+                i = x;
+                x = d;
+                d = v - t * x;
+                v = x;
+            }
+            v %= n;
+            if (v < 0) v = (v + n) % n;
+            return v;
+
+        }
+        //Aqui ocorrerá a divisão Polinomial dos números
+        public static BigInteger divPoly(BigInteger a, BigInteger b, BigInteger c)
+        {
+            BigInteger resto = 0;
+            BigInteger divisor = c;
+            BigInteger aux = a;
+            BigInteger dividendo = 0;
+            BigInteger quociente = 0;
+            for (int i = 1; i < b; i++)
+            {
+                a = a * aux;
+                quociente = a / c;
+                resto = a % c;
+                dividendo = (quociente * divisor) + resto;
+                dividendo = dividendo % c;
+            }
+            return dividendo;
+        }
+        public static BigInteger totTiente(BigInteger a, BigInteger b)
+        {
+            BigInteger phy = (a - 1) * (b - 1);
+            return phy;
+        }
+        public static BigInteger multiN(BigInteger a, BigInteger b)
+        {
+            BigInteger n = 0;
+            BigInteger aux = a;
+            for (int i = 1; i < b; i++)
+            {
+                a = aux * b;
+            }
+            return a;
+        }
+        public static BigInteger eValue(BigInteger tot)
+        {
+            BigInteger ef = 0;
+            for (int i = 1; i < tot; i++)
+            {
+                if (tot % i != 0)
+                {
+                    ef = i;
+                    break;
+                }
+            }
+            return ef;
+        }
+        public static string conversao(string msg)
+        {
+            string conv = "";
+            for(int i=msg.Length-1;i>=0;i--)
+            {
+                conv = conv + msg[i];
+            }
+            return conv;
+        }
+
+        private void botao1_Click(object sender, EventArgs e)
+        {
+            msg1 = text1.Text;
+            p = pPrime((int)numer.Value);
+            q = qPrime((int)numer.Value);
+            n = p * q;
+            totiente = totTiente(p,q);
+            ev = eValue(totiente);
+            keye.Text = Convert.ToString(ev);
+            keyn.Text = Convert.ToString(n);
+            keyp.Text = Convert.ToString(p);
+            keyq.Text = Convert.ToString(q);
+            inv = modInverse(ev, totiente);
+            keyde.Text = Convert.ToString(inv);
+            aloc = new double[msg1.Length];
+            char[] carac = new char[msg1.Length];
+            msgcnv1 = conversao(msg1);
+            for (int i = 0; i < msgcnv1.Length; i++)
+            {
+                ascII1 = (int)msgcnv1[i];
+                cript = divPoly(ascII1, ev, n);
+                aloc[i] = (double)cript;
+                carac[i] = (char)aloc[i];
+                text2.Text += Convert.ToString(carac[i]);
+            }
+        }
+
+        private void botao2_Click(object sender, EventArgs e)
+        {
+            msg2 = text2.Text;
+            char[] carac = new char[msg2.Length];
+            string msg3 = "";
+            for (int i = 0; i < aloc.Length; i++)
+            {
+                ascII2 = (int)aloc[i];
+                desc = divPoly(ascII2, inv, n);
+                carac[i] = (char)desc;
+                msg3 += conversao(Convert.ToString(carac[i]));
+
+            }
+            text3.Text = conversao(msg3);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void numer_ValueChanged(object sender, EventArgs e)
+        {
+            numer.Minimum = 10;
+            numer.Maximum = 100;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            text1.Text = "";
+            text2.Text = "";
+            text3.Text = "";
+
+        }
+
+        private void e_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
